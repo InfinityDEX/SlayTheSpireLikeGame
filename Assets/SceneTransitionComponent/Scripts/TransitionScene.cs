8c3f7af1facca8bc6d0783e62e114938ae1d96f4
kurama <itumonikoniko365@gmail.com>
2026-09-25 01:48:36 +0900

    SceneTransitionComponentのコードをリファクタリング

--- Assets/SceneTransitionComponent/Scripts/TransitionScene.cs
+++ Assets/SceneTransitionComponent/Scripts/TransitionScene.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 
-// UnityのUI（Button）で画面遷移したい場合にGameObjectに本コンポーネントを
-// アタッチしてシーン移動のメソッドを呼び出す。
+/// <summary>
+/// シーン移動クラス
+/// 
+/// UnityのUI（Button）で画面遷移したい場合にGameObjectに本コンポーネントを
+/// アタッチしてシーン移動のメソッドを呼び出す。
+/// </summary>
 public class TransitionScene : MonoBehaviour
 {
+    /// <summary>
+    /// タイトルシーンに移動
+    /// </summary>
     public void GoToTitleScene()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("TitleScene", UnityEngine.SceneManagement.LoadSceneMode.Single);
     }
 
+    /// <summary>
+    /// マップシーンに移動
+    /// </summary>
     public void GoToMapScene()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("MapScene", UnityEngine.SceneManagement.LoadSceneMode.Single);
